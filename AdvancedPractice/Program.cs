b2fa80fe9364ec1d6936a03daea5002eba7b9493
Welt <Welt@DESKTOP-8TGMBPR>
2025-02-06 10:07:00 +0800

    添加项目文件。

---
+++ AdvancedPractice/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int count = 1_000_000;
+
+        // ArrayList测试
+        var arrayList = new ArrayList();
+        var sw = Stopwatch.StartNew();
+        for (int i = 0; i < count; i++)
+        {
+            arrayList.Add(i); // 装箱发生
+        }
+        sw.Stop();
+        Console.WriteLine($"ArrayList添加耗时：{sw.ElapsedMilliseconds}ms");
+
+        // List<int>测试
+        var genericList = new List<int>();
+        sw.Restart();
+        for (int i = 0; i < count; i++)
+        {
+            genericList.Add(i); // 无装箱
+        }
+        sw.Stop();
+        Console.WriteLine($"List<T>添加耗时：{sw.ElapsedMilliseconds}ms");
+    }
+}
---
+++ AdvancedPractice/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedPractice
+{
+    public class Repository<T> where T : class
+    {
+        public readonly List<T> _items = new List<T>();
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+        public T Get(int id)
+        {
+            return _items.FirstOrDefault(e => (int)e.GetType().GetProperty("Id").GetValue(e) == id);
+        }
+        public void Delete(int id)
+        {
+            var item = Get(id);
+            if (item != null)
+            {
+                _items.Remove(item);
+            }
+        }
+    }
+}
