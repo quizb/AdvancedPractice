6d231c5173b7998f6f9f492d2103f605450bff72
quizzz <1316094904@qq.com>
2025-02-07 15:41:37 +0800

    泛型，装箱与不装箱的比较，

--- AdvancedPractice/Program.cs
+++ AdvancedPractice/Program.cs
@@ -2,31 +2,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        const int count = 1_000_000;
+        #region
+        var mydog = new Dog();
+        mydog.Name = "旺财";
+        mydog.Eat();
+        mydog.barking();
 
-        // ArrayList测试
-        var arrayList = new ArrayList();
-        var sw = Stopwatch.StartNew();
-        for (int i = 0; i < count; i++)
-        {
-            arrayList.Add(i); // 装箱发生
-        }
-        sw.Stop();
-        Console.WriteLine($"ArrayList添加耗时：{sw.ElapsedMilliseconds}ms");
+        var mycat = new Cat();
+        mycat.Speak();
 
-        // List<int>测试
-        var genericList = new List<int>();
-        sw.Restart();
-        for (int i = 0; i < count; i++)
-        {
-            genericList.Add(i); // 无装箱
-        }
-        sw.Stop();
-        Console.WriteLine($"List<T>添加耗时：{sw.ElapsedMilliseconds}ms");
+        #endregion
+
+
+
+        #region 泛型，装箱与不装箱的比较
+        //const int count = 1_000_000;
+
+        //// ArrayList测试
+        //var arrayList = new ArrayList();
+        //var sw = Stopwatch.StartNew();
+        //for (int i = 0; i < count; i++)
+        //{
+        //    arrayList.Add(i); // 装箱发生
+        //}
+        //sw.Stop();
+        //Console.WriteLine($"ArrayList添加耗时：{sw.ElapsedMilliseconds}ms");
+
+        //// List<int>测试
+        //var genericList = new List<int>();
+        //sw.Restart();
+        //for (int i = 0; i < count; i++)
+        //{
+        //    genericList.Add(i); // 无装箱
+        //}
+        //sw.Stop();
+        //Console.WriteLine($"List<T>添加耗时：{sw.ElapsedMilliseconds}ms");
+        #endregion
+    }
+
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public void Eat() { Console.WriteLine($"{Name} is eating "); }
+         public virtual void Speak() { Console.WriteLine("Animal sound"); }
+    }
+    
+    public class Dog : Animal 
+    {
+        public void barking() { Console.WriteLine("Wow! Wow!"); }
+    }
+
+    public class Cat : Animal
+    {
+        public override void Speak() { Console.WriteLine("Meow");base.Speak(); }
     }
 }
